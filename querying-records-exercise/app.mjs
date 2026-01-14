9db8cb5ae65714450622d8e93a958725b6e8ac10
Supapond <supapondmaliwab@gmail.com>
2026-01-14 20:35:48 +0700

    (edit)fix: add code .get/movies

--- querying-records-exercise/app.mjs
+++ querying-records-exercise/app.mjs
@@ -10,7 +10,7 @@ app.get("/movies", async (req, res) => {
 	
 	 try {
 	const result = await pool.query(`SELECT * FROM movies`);
-	return res.status(13200).json({
+	return res.status(200).json({
 		data: result.rows,
 	});
    } catch (error){
