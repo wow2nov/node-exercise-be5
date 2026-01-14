04021a49e6021c9ef30cc0a5bba81cf98ada4d34
Supapond <supapondmaliwab@gmail.com>
2026-01-14 16:22:59 +0700

    feat: add code .get/movies

--- querying-records-exercise/app.mjs
+++ querying-records-exercise/app.mjs
@@ -16,6 +16,30 @@ app.get("/movies", async (req, res) => {
 
 // 📍 **** สร้าง API เพื่อใช้ในการดูข้อมูลหนังแต่ละเรื่องด้วย movieId ตรงนี้ ****
 
+app.get("/movies/:movieId", async (req, res)=>{
+	try {
+	const movieIdFromClient = req.params.movieId;
+		const results = await pool.query(`SELECT * FROM movies WHERE movie_id = $1`,
+		[movieIdFromClient]
+	);
+
+	if(!results.rows[0]) {
+		return res.status(404).json({
+		message:"Movie not found",
+		});
+	};
+
+	return res.status(200).json({
+		data : results.rows[0],
+	});
+	} catch (error) {
+		return res.status(500).json({
+		  message: "ไม่สามารถเชื่อมต่อ Database ได้",
+		});
+
+	}
+});
+
 app.listen(port, () => {
 	console.log(`🚀 Server is running at ${port}`);
 });
