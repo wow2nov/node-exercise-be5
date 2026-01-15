c2ffdfb70251be2ef4afe14af92f18a7ba0064b9
Supapond <supapondmaliwab@gmail.com>
2026-01-15 22:29:28 +0700

    feat:add code .get/genres

--- querying-all-records-with-query-parameters-exercise-1/app.mjs
+++ querying-all-records-with-query-parameters-exercise-1/app.mjs
@@ -9,23 +9,33 @@ app.use(express.json());
 app.get("/movies", async (req, res) => {
 	try {
 		// แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างล่างนี้ 🔽🔽🔽
-		const genresParam = null;
+		const genresParam = req.query.genres || null;
 		const result = await pool.query(
 			`
     SELECT * FROM movies
+	WHERE ($1 IS NULL OR genres = $1)
     `,
-			[]
+			[genresParam]
 		);
+
+		if(result.rows.length === 0) {
+			return res.status (404).json ({
+				message : "movie not found",
+			});
+		};
+
 		// แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างบนนี้ 🔼🔼🔼
 
-		return res.json({
+		return res.status(200).json({
 			data: result.rows,
 		});
+
 	} catch (e) {
-		return res.json({
+		return res.status(500).json({
 			message: "ไม่สามารถเชื่อมต่อ Database ได้",
 		});
 	}
+
 });
 
 app.listen(port, () => {
