d0bca273d0f3d50daf4e54ff5a4d60ba1ab880a0
Supapond <supapondmaliwab@gmail.com>
2026-01-21 22:47:30 +0700

    refactor:code pagination

--- pagination-exercise/app.mjs
+++ pagination-exercise/app.mjs
@@ -11,24 +11,30 @@ app.get("/movies", async (req, res) => {
 		// แก้ไขโค้ดเพื่อทำ Pagination ได้ข้างล่างนี้ 🔽🔽🔽
 		const genres = req.query.genres;
 		const keywords = req.query.keywords;
+		const page = req.query.page || 1;
 
-		let query = "select * from movies";
+		let query = ' select * from movies ';
 		let values = [];
 
+		const PAGE_SIZE = 5;
+		const offset = (page - 1) * PAGE_SIZE
+
 		if (keywords && genres) {
-			query += " where genres ilike $1 and title ilike $2";
-			values = [`%${genres}%`, `%${keywords}%`];
+			query += ' where genres ilike $1 and title ilike $2 LIMIT $3 OFFSET $4 ';
+			values = [`%${genres}%`, `%${keywords}%`, PAGE_SIZE , offset];
 		} else if (keywords) {
-			query += " where keywords ilike $1";
-			values = [`%${keywords}%`];
+			query += ' where keywords ilike $1 LIMIT $2 OFFSET $3 ';
+			values = [`%${keywords}%`, PAGE_SIZE , offset];
 		} else if (genres) {
-			query += " where genres ilike $1";
-			values = [`%${genres}%`];
+			query += ' where genres ilike $1 LIMIT $2 OFFSET $3 ';
+			values = [`%${genres}%`, PAGE_SIZE , offset];
+		} else {
+			query += ' LIMIT $1 OFFSET $2 ';
+			values = [ PAGE_SIZE , offset ];
 		}
 		// แก้ไขโค้ดเพื่อทำ Pagination ได้ข้างบนนี้ 🔼🔼🔼
 
-		const result = await pool.query(query, values);
-
+	const result = await pool.query(query, values);
 		return res.json({
 			data: result.rows,
 		});
